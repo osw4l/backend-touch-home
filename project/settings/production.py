b550b3a93788561ed29eea462b17a62fb63fa693
0sw4l <ioswxd@gmail.com>
2018-05-28 03:48:50 -0500

    update app

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,58 +1,27 @@
 from .base import *
 
-DEBUG = False
-
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
+DEBUG = True
 
 INSTALLED_APPS += ('gunicorn',)
 
 ALLOWED_HOSTS = ['*']
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+   
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 WSGI_APPLICATION = 'project.wsgi.production.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '../../project/logs/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'project',
-        'USER': 'project',
-        'PASSWORD': 'project',
-        'HOST': '127.0.0.1',
+        'NAME': 'db5m2khft89et6',
+        'USER': 'fdimxosxljvsca',
+        'PASSWORD': 'd480fd7e379f281793503e7d9ad011caa2ec32a5ef6442dc456873affba369ec',
+        'HOST': 'ec2-75-101-142-91.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
